5558d3075ed5aab4122d8ca6f4b51d11792e0fee
divineProjects <divinejigu@gmail.com>
2025-03-09 03:23:09 +0200

    final

---
+++ scripts/getdates.js
@@ -0,0 +1,14 @@
+// Purpose: Get the current year and update the footer with it
+const domYearElement= document.querySelector("#currentyear");
+// create date object
+const today = new Date();
+const currentYear = today.getFullYear();
+domYearElement.textContent = currentYear;
+
+// Get the last modified date and update footer with it
+const domLastModifiedElement= document.querySelector("#lastModified");
+domLastModifiedElement.textContent  = `Last Modified: ${today.toLocaleDateString()}`;
+
+
+
+
