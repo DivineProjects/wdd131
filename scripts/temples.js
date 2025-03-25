d7c2e57214da51ecdd3afacb2e94e0c71537cd3f
divineProjects <divinejigu@gmail.com>
2025-03-25 05:08:43 +0200

    week02

---
+++ scripts/temples.js
@@ -0,0 +1,26 @@
+// // Select all navigation links
+// const navLinks = document.querySelectorAll('.nav-link');
+// navLinks.forEach(link => {
+//     link.addEventListener('click', function() {
+//         // Remove the 'active' class from all links
+//         navLinks.forEach(link => link.classList.remove('active'));
+
+//         // Add the 'active' class to the clicked link
+//         this.classList.add('active');
+//     });
+// });
+
+
+// // Purpose: Get the current year and update the footer with it
+// const domYearElement= document.querySelector("#currentyear");
+// const today = new Date();
+// const currentYear = today.getFullYear();
+// domYearElement.textContent = currentYear;
+
+// // Get the last modified date and update footer with it
+// const domLastModifiedElement= document.querySelector("#lastModified");
+// domLastModifiedElement.textContent  = `Last Modified: ${today.toLocaleDateString()}`;
+
+
+
+
