00e0358b3fdf539a8093c75421b0004839af2b70
divineProjects <divinejigu@gmail.com>
2025-09-05 17:33:18 +0200

    w01 update

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -11,5 +11,11 @@ const domLastModifiedElement= document.querySelector("#lastModified");
 domLastModifiedElement.textContent  = `Last Modified: ${today.toLocaleDateString()}`;
 
 
+const hamburger = document.getElementById("hamburger");
+const navMenu = document.querySelector(".nav-menu");
+
+hamburger.addEventListener("click", () => {
+  navMenu.classList.toggle("active");
+});
 
 
